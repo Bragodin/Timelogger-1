f6a785f4c4096aa4c17f0b0a41fcf917beab0ef3
Drozd Egor <drozdegor.sr@gmail.com>
2019-04-29 12:35:39 +0300

    donutCart change

--- src/app/donut-chart/donut-chart.component.ts
+++ src/app/donut-chart/donut-chart.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Chart } from 'chart.js';
+import { items } from './donutChartDate';
 
 @Component({
   selector: 'app-donut-chart',
@@ -7,32 +8,62 @@ import { Chart } from 'chart.js';
   styleUrls: ['./donut-chart.component.sass']
 })
 export class DonutChartComponent implements OnInit {
-  DoughnutChart: any = [];
+  DoughnutChart;
+  realResult: number;
+  expectedResult: number;
+  h: string = '';
+
+  constructor(){}
   ngOnInit(){
+    let data = {
+      labels: [],
+      datasets: [{
+        data: [],
+        backgroundColor: [],
+        borderWidth: 0,
+    }]
+      }
+      for(let i = 0; i < items.length; i++){
+        data.labels.push(items[i].name);
+        data.datasets[0].backgroundColor.push(items[i].color);
+        data.datasets[0].data.push(items[i].value);
+      }
+    Chart.defaults.global.legend.labels.usePointStyle = true;
     this.DoughnutChart = new Chart('doughnutChart', {
       type: 'doughnut',
-      data: {
-      labels: ["Office", "Miffort cour...", "Untracked"],
-      datasets: [{
-        data: [9, 7, 3],
-        backgroundColor: [
-            '#FFD600',
-            '#FF0000',
-            '#C4C4C4',
-        ],
-        borderWidth: 1,
-    }]
-      },
+      data: data,
       options: {
-        cutoutPercentage: 80,
+        cutoutPercentage: 90,
         legend: {
-          position: 'bottom',
+          position: 'right',
         },
         animation: {
           animateScale: true,
           animateRotate: true,
         }
 
+      }
+    });
+    let originalDoughnutDraw = Chart.controllers.doughnut.prototype.draw;
+    Chart.helpers.extend(Chart.controllers.doughnut.prototype, {
+      draw: function() {
+        originalDoughnutDraw.apply(this, arguments);
+
+        const chart = this.chart;
+        const width = chart.width,
+            height = chart.height,
+            ctx = chart.ctx;
+
+        var fontSize = (height / 114).toFixed(2);
+        ctx.font = fontSize + "em sans-serif";
+        this.realResult = 10;
+        this.expectedResult = 20;
+        this.h = '';
+        let sum =  this.realResult + '/' + this.expectedResult + this.h;
+        let text = sum, textX, textY;
+        textX = height / 2;
+        textY = height / 2;
+        ctx.fillText(text, textX, textY);
       }
     });
   }
