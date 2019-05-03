94bbf769521857a004404ba96a03910aa6968e7f
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-05-03 12:49:00 +0300

    add filter

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from "@angular/material";
 
 export interface PeriodicElement {
   name: string;
   position: number;
+
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Skype'},
+  {position: 1, name: 'Skype', },
   {position: 2, name: 'Windows'},
   {position: 3, name: 'Word'},
 
@@ -20,8 +22,10 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class ProjectComponent implements OnInit {
 
   displayedColumns: string[] = ['position', 'name'];
-  dataSource = ELEMENT_DATA;
-
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
   constructor() { }
 
   ngOnInit() {
